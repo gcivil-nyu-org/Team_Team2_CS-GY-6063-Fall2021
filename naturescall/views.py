a2a9267a49f67c86ada32d4dda276101081c97c8
Divya Gupta <divyagupta0928@gmal.com>
2021-11-29 16:11:15 -0500

    map legend

--- naturescall/views.py
+++ naturescall/views.py
@@ -42,10 +42,10 @@ def index(request):
 def search_restroom(request):
     context = {}
     if request.GET.get("searched") is not None:
-        location = request.GET["searched"]
+        #location = request.GET["searched"]
         if not request.user.is_authenticated:
             map = str(os.getenv("map"))
-            # location = request.GET["searched"]
+            location = request.GET["searched"]
             tableFilter = RestroomFilter()
             k = search(api_key, '"restroom","food","public"', location, 20)
             data = []
@@ -82,7 +82,7 @@ def search_restroom(request):
             return render(request, "naturescall/search_restroom.html", context)
         else:
             dbRestroom = Restroom.objects.all()
-            # location1 = request.GET["searched"]
+            location = request.GET["searched"]
             profile = request.user.profile
             d = {
                 "accessible": profile.accessible,
@@ -423,6 +423,7 @@ def google_url(loc, loc1, width, height, center, key, maptype="roadmap"):
     gmap_url = "https://maps.googleapis.com/maps/api/staticmap"
     size_str = str(width) + "x" + str(height)
     markers_objects = []
+    center= center+"+NY"
     obj = style_marker(center, style_options={"color": "red", "label":"S"})
     markers_objects.append(obj)
     for loc2 in loc:
--- teamtwo/settings.py
+++ teamtwo/settings.py
@@ -136,9 +136,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = str(os.environ.get("EMAIL_USER"))
-EMAIL_HOST_PASSWORD = str(os.environ.get("EMAIL_PASSWORD"))
-print(EMAIL_HOST_PASSWORD)
+EMAIL_HOST_USER = str(os.getenv("EMAIL_USER"))
+EMAIL_HOST_PASSWORD = str(os.getenv("EMAIL_PASSWORD"))
 EMAIL_PORT = 587
 
 # Activate Django-Heroku.
